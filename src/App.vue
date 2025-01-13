58ce8fc5deb189b6d6681ec7f167a4e2ebd62691
thiagodeas <sousaalvesth@gmail.com>
2025-01-13 11:43:27 -0300

    initializing project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Hero from './components/hero/Hero.vue'
+
+</script>
+
+<template>
+    <Hero />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/hero/Hero.vue
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<template>
+    <div>
+        <h1>Essa é a Hero Page!</h1>
+    </div>
+</template>
